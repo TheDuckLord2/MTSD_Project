81cd58cf945e411d9169afe1aebed6ac06119722
TheDuckLord2 <88846468+TheDuckLord2@users.noreply.github.com>
2023-04-12 16:24:06 -0500

    Create cart.py

---
+++ cart.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+class cart.py:
+		cartID = None
+		itemID = None
+		cartTotal = None
+		def __init__(cartID, itemID, cartTotal):
+			self.cartID = cartID
+			self.itemID = itemID
+			self.cartTotal = 0
+		
+		

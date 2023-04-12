60f4735c637cb1f541b18b642e61711c24c26e75
sbusey <129888916+sbusey@users.noreply.github.com>
2023-04-12 17:00:41 -0500

    Create user.py

---
+++ user.py
@@ -0,0 +1,63 @@
+import sqlite3 #Importing the module
+import pandas as pd
+
+cnxn = sqlite2.connect('
+
+conn = sqlite3.connect("MTSD_Database.db") # database object
+
+curr = conn.cursor() # cursor object
+
+curr.execute(table command)
+
+conn.commit()
+
+data = createAccount()
+addData = """INSERT INTO Users VALUES("First Name", "Last Name", "UserID", "Address", "Password","Username","Email","Phone Number","Payment Info")"""
+
+class user:
+  
+  def create_connection("First Name","Last Name","UserID","Address","Password","Username","Email","Phone Number","Payment Info"):
+    connection = None
+    try:
+      connection = MTSD_Database.connector.connect(
+        firstName = "First Name",
+        lastName = "Last Name",
+        userID = "UserID",
+        address = "Address",
+        password = "Password",
+        username = "Username",
+        email = "Email",
+        phoneNumber = "Phone Number",
+        paymentInfo = "Payment Info",
+      )
+  
+  MTSD_Database = importname.connector.connect(
+  
+  firstName = "First Name",
+  lastName = "Last Name",
+  userId = "UserID",
+  address = "Address",
+  password = "Password",
+  username = "Username",
+  email = "Email",
+  phoneNumber = "Phone Number",
+  paymentInfo = "Payment Info",
+  
+  # )
+  
+  def user():
+    
+  def user(firstName, lastName, userID, address, password, username, email, phoneNumber, paymentInfo):
+  
+  def viewProfile():
+    print "Name: " + firstName + " " + lastName
+    print "User ID: " + userID
+    print "Username: " + username
+    print "Email: " + email
+    print "Phone Number: " + phoneNumber
+    print "Address: " + address
+    print "Payment Information: " + paymentInfo
+    
+  def setPassword():
+    
+  

b27953f58c07319dcbada76196b0f543a223bde7
Hunter Pope <130607231+coolcelary@users.noreply.github.com>
2023-04-12 17:00:39 -0500

    Update OrderHistory.py

--- OrderHistory.py
+++ OrderHistory.py
@@ -1,2 +1,15 @@
 import cart
+import sqlite3
 
+conn = sqlite3.connect('example.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    
+    def getOrderID:
+      
+    def getUserID:
+      
+    def getOrderDate:
+      
+    def getTotalPrice:

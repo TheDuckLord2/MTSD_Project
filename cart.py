20e89ea5f03a627520930915d9dd869b6e3d6cb0
TheDuckLord2 <88846468+TheDuckLord2@users.noreply.github.com>
2023-04-12 17:00:37 -0500

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,17 +1,28 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  cartID = "Cart ID",
-  itemID = "Item ID",
-  itemName = "Item Name",
-  itemQuantity = "Item Quantity",
-  itemPrice = "Item Price"
+	cartID = "Cart ID",
+	itemID = "Item ID",
+	itemName = "Item Name",
+	itemQuantity = "Item Quantity",
+	itemPrice = "Item Price"
 )
 class cart:
-		cartID = None
-		itemID = None
-		cartTotal = None
-		def __init__():
-			cartID = mydb.cartID
-			itemID = mydb.itemID
-			cartTotal = 0
+	cartID = None
+	itemID = None
+	cartTotal = None
+	def __init__():
+		cartID = mydb.cartID
+		itemID = mydb.itemID
+		cartTotal = 0
+	def addItem(amt):
+		#connect to database
+		#take selected item and add to cart table
+	def removeItem(amt):
+		#connect to database
+		#remove selected item from cart table based on specific amount
+	def checkout():
+		#display total
+		#send order information to orderHistory DB
+		#remove all items from cart
+	

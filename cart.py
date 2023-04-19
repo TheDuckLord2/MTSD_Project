a3d1600754ac93cf1212c26162164c606474cd3b
sbusey <129888916+sbusey@users.noreply.github.com>
2023-04-19 17:13:02 -0500

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,13 +1,19 @@
-import mysql.connector
+#import mysql.connector
+import sqlite3
 
-mydb = mysql.connector.connect(
-	cartID = "Cart ID",
-	itemID = "Item ID",
-	itemName = "Item Name",
-	itemQuantity = "Item Quantity",
-	itemPrice = "Item Price"
+#mydb = mysql.connector.connect(
+	#cartID = "Cart ID",
+	#itemID = "Item ID",
+	#itemName = "Item Name",
+	#itemQuantity = "Item Quantity",
+	#itemPrice = "Item Price"
 )
 class cart:
+	
+	conn = sqlite3.connect('MTSD_Database.db')
+	cursor = conn.cursor()
+	
+	
 	cartID = None
 	itemID = None
 	cartTotal = None
@@ -16,13 +22,41 @@ class cart:
 		itemID = mydb.itemID
 		cartTotal = 0
 	def addItem(amt):
-		#connect to database
-		#take selected item and add to cart table
-	def removeItem(amt):
+		#connect to database 
+		whichItem = input("Enter the item name: ")
+		howMuch = amt;
+		newCartID = new_cartID()
+		
+		currQuantity = cursor.execute("SELECT Item Quantity FROM Inventory WHERE Item Name = ?",(whichItem))
+		while howMuch > currQuantity
+			howMuch = input("There are not that many items in stock. Try again. Quantity: ")
+			currQuantity = cursor.execute("SELECT Item Quantity FROM Inventory WHERE Item Name = ?",(whichItem))
+			
+		cursor.execute("SELECT * FROM Inventory WHERE Item Name = ?",(whichItem))
+		for i in range(howMuch)
+			cursor.execute("INSERT INTO Cart SELECT * FROM Inventory WHERE Item Name = ? AND INSERT INTO Cart VALUES ?", (whichItem, newCartID))
+
 		#connect to database
 		#remove selected item from cart table based on specific amount
+				       
 	def checkout():
-		#display total
+		#display total -> function?
+		
+		whichCart = input("Enter your cartID to checkout: ")
+		
+		
 		#send order information to orderHistory DB
+		cursor.execute("INSERT INTO Order History SELECT * FROM Cart WHERE Cart ID = ?", (whichCart))
+		
 		#remove all items from cart
+		cursor.execute("DELETE * FROM Cart WHERE Cart ID = ?",(whichCart))
+		# might not need the *
+				   
+	 def new_cartID():
+   		cartID++
+   		return cartID		
+	
+	
+	conn.commit()
+	conn.close()
 	

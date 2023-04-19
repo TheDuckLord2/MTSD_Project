9ee3f6bedb2fd8036b2ae4fa7c6eea4c056f804e
Hunter Pope <130607231+coolcelary@users.noreply.github.com>
2023-04-19 17:19:55 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -16,18 +16,3 @@ class inventory
     def removeItem(ItemID, Quantity):
         c.execute("UPDATE Inventory SET Quantity=? WHERE ItemID =?",(Quantity, ItemID)
         conn.commit()
-    def getItemID:
-                  
-    def getItemName:              
-        
-    def getItemQuantity:
-    
-    def getItemPrice:
-                  
-    def setItemID:
-                  
-    def setItemName:
-                  
-    def setItemQuantity:
-                  
-    def setItemPrice:

c17509882f6ddef2427895021d5c313ddc861acd
Hunter Pope <130607231+coolcelary@users.noreply.github.com>
2023-04-19 17:12:50 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,4 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+
+c = conn.cursor()
+
 class inventory
+    
     itemID
     itemName
     itemQuantity
@@ -6,4 +13,21 @@ class inventory
 
     def inventory():
     
+    def removeItem(ItemID, Quantity):
+        c.execute("UPDATE Inventory SET Quantity=? WHERE ItemID =?",(Quantity, ItemID)
+        conn.commit()
+    def getItemID:
+                  
+    def getItemName:              
         
+    def getItemQuantity:
+    
+    def getItemPrice:
+                  
+    def setItemID:
+                  
+    def setItemName:
+                  
+    def setItemQuantity:
+                  
+    def setItemPrice:

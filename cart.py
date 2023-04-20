8bc92fe20e55632123a41fcbddc08db6e89b88d2
TheDuckLord2 <88846468+TheDuckLord2@users.noreply.github.com>
2023-04-19 23:00:36 -0500

    Update cart.py

--- cart.py
+++ cart.py
@@ -28,23 +28,28 @@ class cart:
 		newCartID = new_cartID()
 		
 		currQuantity = cursor.execute("SELECT Item Quantity FROM Inventory WHERE Item Name = ?",(whichItem))
+		quantityDiff = currQuantity - howMuch
 		while howMuch > currQuantity
 			howMuch = input("There are not that many items in stock. Try again. Quantity: ")
 			currQuantity = cursor.execute("SELECT Item Quantity FROM Inventory WHERE Item Name = ?",(whichItem))
-			
+			quantityDiff = currQuantity - howMuch
 		cursor.execute("SELECT * FROM Inventory WHERE Item Name = ?",(whichItem))
 		for i in range(howMuch)
 			cursor.execute("INSERT INTO Cart SELECT * FROM Inventory WHERE Item Name = ? AND INSERT INTO Cart VALUES ?", (whichItem, newCartID))
-
+			addTotal(whichItem)
 		#connect to database
+		if quantityDiff > 0:
+			cursor.excecute("UPDATE Inventory SET Item Quantity ? WHERE Item Name = ?",(quantityDiff,whichItem))
 		#remove selected item from cart table based on specific amount
-				       
+		elif quantityDiff == 0:
+			cursor.execute("DELETE * FROM Inventory WHERE Item Name = ?"(whichItem))
+		#or if the item doesn't get deleted:remove the if and elif, just leaving the first SQL statement
 	def checkout():
 		#display total -> function?
 		
 		whichCart = input("Enter your cartID to checkout: ")
 		
-		
+		print("Order total: $" + cartTotal)
 		#send order information to orderHistory DB
 		cursor.execute("INSERT INTO Order History SELECT * FROM Cart WHERE Cart ID = ?", (whichCart))
 		
@@ -56,7 +61,8 @@ class cart:
    		cartID++
    		return cartID	
 	
-	#def addTotal():
+	def addTotal(item):
+		cartTotal += cursor.execute("SELECT Item Price FROM Inventory WHERE Item Name = ?",(item))
 		
 	
 	

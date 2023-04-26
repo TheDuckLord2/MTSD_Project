09dc3f2b78001cfc9da8aa05a382f6ff56c4c571
Hunter Pope <130607231+coolcelary@users.noreply.github.com>
2023-04-26 16:52:03 -0500

    Update OrderHistory.py

--- OrderHistory.py
+++ OrderHistory.py
@@ -3,7 +3,7 @@ import sqlite3
 
 conn = sqlite3.connect('MTSD_Database.db')
 cursor = conn.cursor()
-#commit fa
+#commit f
 class OrderHistory:
     def __init__(self) -> None:
         orderID = None

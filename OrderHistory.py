7a19912a15ef08d4dcb8d62e3c1cd3111b9dcc99
Hunter Pope <130607231+coolcelary@users.noreply.github.com>
2023-04-26 16:56:08 -0500

    Update OrderHistory.py

--- OrderHistory.py
+++ OrderHistory.py
@@ -22,3 +22,293 @@ class OrderHistory:
         return self.orderDate
     def getTotalPrice(self):
         return self.totalPrice
+    
+    import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPriceimport cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+import cart
+import sqlite3
+
+conn = sqlite3.connect('MTSD_Database.db')
+cursor = conn.cursor()
+
+class OrderHistory:
+    def __init__(self) -> None:
+        orderID = None
+        userID = None
+        orderDate = None
+        totalPrice = None
+    def __init__(self, uID, oD, tP):
+        self.userID = uID
+        self.orderDate = oD
+        self.totalPrice = tP
+    def getOrderID(self):
+        return self.orderID
+    def getUserID(self):
+        return self.userID
+    def getOrderDate(self):
+        return self.orderDate
+    def getTotalPrice(self):
+        return self.totalPrice
+

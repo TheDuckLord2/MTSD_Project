8953f59532896b285d914b5fb6e72350fc180748
sbusey <sbusey4904@gmail.com>
2023-04-25 20:54:56 -0500

    add to cart and updating inventory works

--- cart.py
+++ cart.py
@@ -2,8 +2,7 @@ import sqlite3
 conn = sqlite3.connect('MTSD_Database.db')
 cursor = conn.cursor()
 class cart:
-	
-	
+
 	def __init__(self):
 		self.cartID = None
 		self.itemID = None
@@ -12,24 +11,32 @@ class cart:
 	def addItem(id, amt):
 		#connect to database 
 		whichItem = id
-		howMuch = amt;
+		howMuch = int(amt)
 		#newCartID = cart.new_cartID()
 		
-		currQuantity = cursor.execute("SELECT Item Quantity FROM Inventory WHERE Item ID = ?",(whichItem))
+		currQuantity = cursor.execute("SELECT Item_Quantity FROM Inventory WHERE ItemID = ?",(whichItem))
+		currQuantity = cursor.fetchone()
+		currQuantity = ''.join(map(str,currQuantity))
+		currQuantity = int(currQuantity)
 		quantityDiff = currQuantity - howMuch
 		while howMuch > currQuantity:
 			howMuch = input("There are not that many items in stock. Try again. Quantity: ")
-			currQuantity = cursor.execute("SELECT Item Quantity FROM Inventory WHERE Item ID = ?",(whichItem))
+			currQuantity = conn.execute("SELECT Item_Quantity FROM Inventory WHERE ItemID = ?",(whichItem))
 			quantityDiff = currQuantity - howMuch
-		cursor.execute("SELECT * FROM Inventory WHERE Item Name = ?",(whichItem))
-		for i in range(howMuch):
-			cursor.execute("INSERT INTO Cart SELECT * FROM Inventory WHERE Item Name = ? AND INSERT INTO Cart VALUES ?", (whichItem))
-			cart.addTotal(whichItem)
-		#connect to database
-		cursor.excecute("UPDATE Inventory SET Item Quantity ? WHERE Item Name = ?",(quantityDiff,whichItem))
+		#conn.execute("SELECT * FROM Inventory WHERE ItemID = ?",(whichItem))
+		conn.execute("INSERT INTO Cart (ItemID,Item_Name,Item_Price,Item_Quantity) SELECT ItemID, Item_Name, Item_Price, ? FROM Inventory WHERE ItemID = ?", (howMuch, whichItem))
+		conn.execute("UPDATE Cart SET Item_Quantity = ? WHERE ItemID = ?",(howMuch, whichItem))
+# HAHA IT WORKS
+# BUT HOW DO WE KNOW WHICH CART IS FOR WHICH USER?????????
+	#	cart.addTotal(whichItem)
+		cart.getTotal()
+
+		conn.execute("UPDATE Inventory SET Item_Quantity = ? WHERE ItemID = ?",(quantityDiff,whichItem))
+		conn.commit()
 
 	def checkout():
 		#display total -> function?
+		cart.getTotal()
 		
 		whichCart = input("Enter your cartID to checkout: ")
 		
@@ -40,8 +47,20 @@ class cart:
 		#remove all items from cart
 		cursor.execute("DELETE * FROM Cart WHERE Cart ID = ?",(whichCart))
 		# might not need the *
-	def addTotal(item):
-		cartTotal += cursor.execute("SELECT Item Price FROM Inventory WHERE Item Name = ?",(item))
+
+
+
+	def getTotal():
+		priceTotal = cursor.execute("SELECT SUM(Item_Price) FROM Cart")
+		priceTotal = cursor.fetchone()
+		return priceTotal
+	
+	# This was causing me some issues so I commented it out sorry
+
+	#def addTotal(item):
+		#price = cursor.execute("SELECT Item_Price FROM Inventory WHERE Item_Name = ?",(item))
+		#price = cursor.fetchone()
+	#	cartTotal += price
 		
 	def displayCart():
 		cursor.execute("SELECT * FROM Inventory")

8fb14df6bd86b09454b7c186cc354795fdcd080d
TheDuckLord2 <88846468+TheDuckLord2@users.noreply.github.com>
2023-04-28 15:57:35 -0500

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -5,12 +5,6 @@ conn = sqlite3.connect('MTSD_Database.db')
 c = conn.cursor()
 
 class inventory:
-    def __init__(self):
-        self.itemID = None
-        self.itemName = None 
-        self.itemQuantity = None
-        self.itemPrice = None
-
     def removeItem(ItemID, Quantity):
         c.execute("UPDATE Inventory SET Quantity=? WHERE ItemID =?",(Quantity, ItemID))
         conn.commit()
